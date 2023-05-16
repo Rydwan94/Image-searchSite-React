88c2b2b164a11f9d5021b82af6da762249f7249f
rydwan94 <rydwan94@gmail.com>
2023-05-16 19:04:07 +0200

    final commit

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -1,29 +1,56 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { ImagesContext } from "../context/ImagesContext";
 
-import '../css/ImageList.css'
+import "../css/ImageList.css";
 
 const ImagesList = () => {
+  const [imageVisible, setImageVisible] = useState(false);
 
-    const {state: {filteredData}, inputValue, dispatch} = useContext(ImagesContext)
+  const showImages = () => {
+    const position = document.documentElement.scrollTop;
+    if (position > 100) {
+      setImageVisible(true);
+    } else if (position <= 100) {
+      setImageVisible(false);
+    }
+  };
 
-    const zoomImage = (id) => {
-        dispatch({type: 'IMAGE_ZOOM', id})
-        console.log('lala')
-      }
+  useEffect(() => {
+    window.addEventListener("scroll", showImages);
 
-    const imageList = filteredData.map(item => (
-        <div onClick={() => zoomImage(item.id)} className={item.isActive? 'zoomedSingleImage' : 'singleImage'} id={inputValue} key={item.id}>
-            <img src={item.src} alt={item.alt} />
-        </div>
-    ))
+    return () => {
+      window.removeEventListener("scroll", showImages);
+    };
+  }, []);
 
-    return ( 
-        <div className="ImagesContainer">
-            {imageList}
-        </div>
-     );
-}
- 
+  const {
+    state: { filteredData },
+    inputValue,
+    dispatch,
+  } = useContext(ImagesContext);
+
+  const zoomImage = (id) => {
+    dispatch({ type: "IMAGE_ZOOM", id });
+    console.log("lala");
+  };
+
+  const imageList = filteredData.map((item) => (
+    <div
+      onClick={() => zoomImage(item.id)}
+      className={item.isActive ? "zoomedSingleImage" : "singleImage"}
+      id={inputValue}
+      key={item.id}
+    >
+      <img
+        className={imageVisible ? "showImages" : ""}
+        src={item.src}
+        alt={item.alt}
+      />
+    </div>
+  ));
+
+  return <div className="ImagesContainer">{imageList}</div>;
+};
+
 export default ImagesList;
